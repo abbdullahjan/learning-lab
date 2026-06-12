f8f632a1d937607a5e19a23978ccdb9919ad94f6
abbdullahjan <ajan83519@gmail.com>
2026-06-12 06:34:02 +0500

    add

---
+++ Python/Random Practice/Problems/Claude/longestStreak.py
@@ -0,0 +1,24 @@
+def longestStreak(num):
+    num.sort()
+    longest = 0
+    pre = 0
+    if len(num) > 0:
+        pre = num[0]
+    currentStreak = 0
+    if len(num) > 1:
+        if num[0] == (num[1] - 1) :
+            currentStreak = 1    
+    i = 1
+    while i < len(num):        
+        if pre == num[i] - 1 :
+            currentStreak += 1
+            if currentStreak > longest:
+                longest = currentStreak
+        else:
+            currentStreak = 0
+        pre = num[i]
+        i+=1
+
+    return longest    
+
+print(longestStreak([100, 4, 200, 1, 3, 2]))
